9d95e2d5a9c4381e484cd64dd299c50f6f45a63d
Syha Ban <syhaban@hotmail.com>
2023-04-05 21:33:54 -0400

    Adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = 1
+Math.floor(Math.random() * 10) + 1;
+
+let num3 = 8;
+let num4 = 2;
+let mod = num3 / num4;
+
+let max = (1, 2, 10, 16, 18, 20);
